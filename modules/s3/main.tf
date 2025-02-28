14375857bb7f9ff075cfb78693b4c78421b1b388
tinhtq <truongquangtinh1997@gmail.com>
2025-03-01 00:07:21 +0700

    update

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -14,7 +14,7 @@ locals {
 
   sse_algorithm     = var.kms_key_arn == "" ? local.sse_s3_algorithm : local.sse_kms_algorithm
   kms_master_key_id = var.kms_key_arn == "" ? null : var.kms_key_arn
-  create_bucket = var.create_bucket
+  create_bucket     = var.create_bucket
 
   create_bucket_acl = (var.acl != null && var.acl != "null") || length(local.grants) > 0
 
@@ -238,8 +238,8 @@ resource "aws_s3_bucket_cors_configuration" "this" {
 resource "aws_s3_bucket_lifecycle_configuration" "this" {
   count = local.create_bucket && length(local.lifecycle_rules) > 0 ? 1 : 0
 
-  bucket                                 = aws_s3_bucket.this[0].id
-  expected_bucket_owner                  = var.expected_bucket_owner
+  bucket                = aws_s3_bucket.this[0].id
+  expected_bucket_owner = var.expected_bucket_owner
   # transition_default_minimum_object_size = var.transition_default_minimum_object_size
 
   dynamic "rule" {
@@ -396,34 +396,35 @@ data "aws_iam_policy_document" "combined" {
 # AWS Load Balancer access log delivery policy
 locals {
   # List of AWS regions where permissions should be granted to the specified Elastic Load Balancing account ID ( https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html#attach-bucket-policy )
-  elb_service_accounts = {
-    us-east-1      = "127311923021"
-    us-east-2      = "033677994240"
-    us-west-1      = "027434742980"
-    us-west-2      = "797873946194"
-    af-south-1     = "098369216593"
-    ap-east-1      = "754344448648"
-    ap-south-1     = "718504428378"
-    ap-northeast-1 = "582318560864"
-    ap-northeast-2 = "600734575887"
-    ap-northeast-3 = "383597477331"
-    ap-southeast-1 = "114774131450"
-    ap-southeast-2 = "783225319266"
-    ap-southeast-3 = "589379963580"
-    ca-central-1   = "985666609251"
-    eu-central-1   = "054676820928"
-    eu-west-1      = "156460612806"
-    eu-west-2      = "652711504416"
-    eu-west-3      = "009996457667"
-    eu-south-1     = "635631232127"
-    eu-north-1     = "897822967062"
-    me-south-1     = "076674570225"
-    sa-east-1      = "507241528517"
-    us-gov-west-1  = "048591011584"
-    us-gov-east-1  = "190560391635"
-    cn-north-1     = "638102146993"
-    cn-northwest-1 = "037604701340"
-  }
+  # elb_service_accounts = {
+  #   us-east-1      = "127311923021"
+  #   us-east-2      = "033677994240"
+  #   us-west-1      = "027434742980"
+  #   us-west-2      = "797873946194"
+  #   af-south-1     = "098369216593"
+  #   ap-east-1      = "754344448648"
+  #   ap-south-1     = "718504428378"
+  #   ap-northeast-1 = "582318560864"
+  #   ap-northeast-2 = "600734575887"
+  #   ap-northeast-3 = "383597477331"
+  #   ap-southeast-1 = "114774131450"
+  #   ap-southeast-2 = "783225319266"
+  #   ap-southeast-3 = "589379963580"
+  #   ca-central-1   = "985666609251"
+  #   eu-central-1   = "054676820928"
+  #   eu-west-1      = "156460612806"
+  #   eu-west-2      = "652711504416"
+  #   eu-west-3      = "009996457667"
+  #   eu-south-1     = "635631232127"
+  #   eu-north-1     = "897822967062"
+  #   me-south-1     = "076674570225"
+  #   sa-east-1      = "507241528517"
+  #   us-gov-west-1  = "048591011584"
+  #   us-gov-east-1  = "190560391635"
+  #   cn-north-1     = "638102146993"
+  #   cn-northwest-1 = "037604701340"
+  # }
+  elb_service_accounts = {}
 }
 
 data "aws_iam_policy_document" "elb_log_delivery" {
