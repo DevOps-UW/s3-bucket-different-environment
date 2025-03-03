072a932ecd84ece33764c32ab5faacf5771ea52e
Trong Truong <trongtruong2509@gmail.com>
2025-03-03 19:52:44 +0700

    change workflow to use OIDC

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "854473044844-myaws-s3"
+    bucket = "011624006725-backed-pmi"
     key    = "tf-state/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
